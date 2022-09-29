b1c2327884ef3a08311ee9e38491ee490a7ce45e
OfelyaSCS <sahakyanscofelya@gmail.com>
2022-09-29 21:29:08 +0400

    stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,21 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
+    double number;
     void divide(int toBeDivided) {
+        number=toBeDivided/5;
     }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter number ");
+        int number=input.nextInt();
+        NumberDivider result=new NumberDivider();
+        result.divide(number);
+        System.out.println("Result number: "+result.number);
+    }
+
+
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,19 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
+    float seconds;
     public void convert(float minutes) {
+        seconds=minutes*60;
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter minutes ");
+        float minutes=input.nextFloat();
+        TimeConvertor result=new TimeConvertor();
+        result.convert(minutes);
+        System.out.println("Seconds: "+result.seconds);
     }
 }
